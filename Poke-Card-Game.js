c8bed53f7cff76125cce822f497fc4b061d538d4
Kevin Dat Nguyen <datkevin@gmail.com>
2019-10-25 14:20:23 -0700

    Implemented rounds into the game. Game will now process with player playing all 3 cards in hand before a round is over. Game is played over 3 rounds or 2 rounds if the player or computer has won 2 - 0

--- Poke-Card-Game.js
+++ Poke-Card-Game.js
@@ -64,6 +64,7 @@ class MakePlayer {
     this.points = 0
     this.currentCardDamage = 0
     this.currentCard = []
+    this.roundsWon = 0
 
   }
 
@@ -97,42 +98,65 @@ let response = ""
 
 var gamestart = function (){
     
-    //Checks if the deck is empty and declares winner!
-    if (deck.length == 0) {
-      console.log("Game over!")
+
+    //When player hands are empty, new round beigns and players are dealt new cards
+    if (player.deck.length == 0) {
+      //Checks to see if a round was won
       if (player.points > computer.points) {
-        console.log("You win! Congratulations " + player.name + "!")
-        console.log("You won " + player.points + " to " + computer.points)
-        console.log("Nice job!")
+        console.log("You won the round!\n")
+        player.roundsWon += 1
       }
       else if (player.points < computer.points) {
+        console.log("Your opponent has won the round!\n")
+        computer.roundsWon += 1
+      }
+      //prevents declaring a tie at the beginning of the game
+      else if (player.currentCardDamage != 0){
+        console.log("It's a tie! No one wins this round!\n")
+      }
+      player.points = 0
+      computer.points = 0
+      player.getDeck()
+      computer.getDeck()
+    }
+
+    //Checks if the deck is empty or if the score is 2 - 0 and declares winner!
+    if ((deck.length == 0 && player.deck.length == 0) || (computer.roundsWon == 2 && player.roundsWon == 0) 
+      || (player.roundsWon == 2 && computer.roundsWon == 0)) {
+      console.log("Game over!")
+      if (player.roundsWon > computer.roundsWon) {
+        console.log("You win! Congratulations " + player.name + "!")
+        console.log("You won " + player.roundsWon + " to " + computer.roundsWon)
+        console.log("Nice job!")
+      }
+      else if (player.roundsWon < computer.roundsWon) {
         console.log("Computer wins! Sorry " + player.name + "!")
-        console.log("You lost " + player.points + " to " + computer.points)
+        console.log("You lost " + player.roundsWon + " to " + computer.roundsWon)
         console.log("You'll do better next time!")
       }
       else {
         console.log("It's a tie!")
-        console.log("Points were " + player.points + " to " + computer.points)
+        console.log("Round points were " + player.roundsWon + " to " + computer.roundsWon)
         console.log("Try harder next time")
       }
       return rl.close()
     } 
 
 
-    //Deal Cards to players
-    player.getDeck()
-    computer.getDeck()
-
     //Computer picks a card
     computer.currentCard[0] = computer.deck[Math.floor(Math.random()*(2))]
     computer.currentCardDamage = computer.currentCard[0].damage
    
     //UI for details as well as prompt that ask for response 
-   rl.question("\nHello " + player.name + "! These are your pokemon: \n\n" 
-      + "1. " + player.deck[0].name + ": " + player.deck[0].damage + " damage \n"
-      + "2. " + player.deck[1].name + ": " + player.deck[1].damage + " damage \n"
-      + "3. " + player.deck[2].name + ": " + player.deck[2].damage + " damage \n"
-      + "\nWhich one do you wanna play? (1, 2, or 3, any other input quits):  \n", 
+    console.log("Player: " + player.name + " -------- Point scores: "
+      + player.points + " - " + computer.points + " -------- Round scores: "
+      + player.roundsWon + " - " + computer.roundsWon 
+      + "\nThese are your pokemon: \n")
+      for (let i = 0; i < player.deck.length; i++) {
+        console.log((i+1) + ". " + player.deck[i].name + ": " + player.deck[i].damage + " damage \n")
+       }
+
+   rl.question("Which one do you wanna play? (pick the corresponding number. Any other input quits):  \n", 
       function(answer) {
         response = answer
 
@@ -142,12 +166,12 @@ var gamestart = function (){
         player.currentCard[0] = (player.deck[0])
         player.deck.splice(player.deck[0], 1)
       } 
-      else if (response == 2){
+      else if (player.deck.length >= 2 && response == 2){
         player.currentCardDamage = player.deck[1].damage
         player.currentCard[0] = (player.deck[1])
         player.deck.splice(player.deck[1], 1)
       }
-      else if (response == 3){
+      else if (player.deck.length >= 3 && response == 3){
         player.currentCardDamage = player.deck[2].damage
         player.currentCard[0] = (player.deck[2])
         player.deck.splice(player.deck[2], 1)
@@ -178,13 +202,12 @@ var gamestart = function (){
         player.points += 1
         console.log("Player points: " + player.points + "\n")
       }
-      else {
+      else if (player.currentCardDamage == computer.currentCardDamage){
         console.log("It's a tie! No one gets any points. \n")
       }
       
-      //clears the decks and deal new cards
-      player.clearDeck()
-      computer.clearDeck()
+      
+      //Loops the game
       gamestart()
     });
 }
